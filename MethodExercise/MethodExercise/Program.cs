2dc9bda7d4ffcab0026b79994ab1029ef0f8602a
bchan31 <59836699+bchan31@users.noreply.github.com>
2020-01-17 13:39:24 -0600

    overloads added

--- MethodExercise/MethodExercise/Program.cs
+++ MethodExercise/MethodExercise/Program.cs
@@ -7,8 +7,13 @@ namespace MethodExercise
         static void Main(string[] args)
         {
             Questions();
-            Sum(3, 2, 54, 66, 31);
+            Add(3, 2, 54, 66, 31);
             Multiply(4, 2, 5);
+
+            Add(5.1m, 7.2m);
+            Add(3, 5, true);
+            Add(1, 0, true);
+            Add(54, 22, false);
         }
 
         public static void Questions()
@@ -25,7 +30,7 @@ namespace MethodExercise
             Console.WriteLine($"My name is {name}. I have a dream of riding a {color} {animal} while listening to {band}.");
         }
 
-        public static void Sum(params int[] list)
+        public static void Add(params int[] list)
         {
             var sum = 0;
             for (int i = 0; i < list.Length; i++)
@@ -33,6 +38,36 @@ namespace MethodExercise
                 sum += list[i];
             }
             Console.WriteLine(sum);
+        }
+
+        public static void Add(params decimal[] list)
+        {
+            var sum = 0m;
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum += list[i];
+            }
+            Console.WriteLine(sum);
+        }
+
+        public static void Add(int x, int y, bool isNeeded)
+        {
+            int answer = x + y;
+            if(isNeeded == true)
+            {
+                if(answer != 1)
+                {
+                    Console.WriteLine(x + y + " dollars");
+                }
+                else
+                {
+                    Console.WriteLine(x + y + " dollar");
+                }
+            }
+            else
+            {
+                Console.WriteLine("It is not needed");
+            }
         }
 
         public static void Multiply(params int[] list)
