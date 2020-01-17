65336f81f563c7fea921a86d60f1410098aff3ab
bchan31 <59836699+bchan31@users.noreply.github.com>
2020-01-17 10:44:41 -0600

    completed method exercises

---
+++ MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"My name is {name}. I have a dream of riding a {color} {animal} while listening to {band}.");
+
+            Console.WriteLine(Sum(3, 2, 54, 66, 31));
+        }
+
+        public static int Sum(params int[] list)
+        {
+            var sum = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                sum += list[i];
+            }
+            return sum;
+        }
+    }
+}
